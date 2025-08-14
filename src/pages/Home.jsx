3909ed90c1df2427ef66dde1f5e01b179f89a044
Leochin1206 <leoaviana1206@gmail.com>
2025-08-14 16:17:52 -0300

    leo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -190,7 +190,7 @@ const CardComando = styled.div`
   border-radius: 12px;
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
   width: 100%;
-  max-width: 550px;
+  max-width: 850px;
   margin-bottom: 20px;
 
   @media (min-width: ${({ theme }) => theme.breakpoints.desktop}) {
